d5eb022dc785fbbb4791c48eeeeec0301842fee1
Ruklić Luka <ruklic.luka@gmail.com>
2015-07-16 00:42:56 +0200

    Implemented exporting to CSV and XLS formats.

--- Quiz/app/services/model/impl/QuestionServiceImpl.java
+++ Quiz/app/services/model/impl/QuestionServiceImpl.java
@@ -1,6 +1,12 @@
 package services.model.impl;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import models.Question;
+import models.User;
+import play.db.jpa.JPA;
 import play.db.jpa.Transactional;
 import services.model.QuestionService;
 
@@ -9,6 +15,14 @@ public class QuestionServiceImpl extends BaseModelServiceImpl<Question> implemen
 
 	protected QuestionServiceImpl() {
 		super(Question.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Question> findQuestionsByAdmin(String username) {
+		Query query = JPA.em().createQuery("SELECT q FROM Question q WHERE q.admin.username = :username", Question.class);
+		query.setParameter("username", username);
+		return query.getResultList();
 	}
 
 }
