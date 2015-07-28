b9c3c7b3f2296a3a04832166e54e3bcfb4a3436a
Ivan Weber <Ivan.Weber@fer.hr>
2015-07-28 21:48:54 +0200

    Added JaroWinkler check for new questions.

--- Quiz/app/services/model/impl/QuestionServiceImpl.java
+++ Quiz/app/services/model/impl/QuestionServiceImpl.java
@@ -1,5 +1,6 @@
 package services.model.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Query;
@@ -9,6 +10,7 @@ import models.User;
 import play.db.jpa.JPA;
 import play.db.jpa.Transactional;
 import services.model.QuestionService;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.JaroWinkler;
 
 @Transactional
 public class QuestionServiceImpl extends BaseModelServiceImpl<Question> implements QuestionService {
@@ -23,6 +25,23 @@ public class QuestionServiceImpl extends BaseModelServiceImpl<Question> implemen
 		Query query = JPA.em().createQuery("SELECT q FROM Question q WHERE q.admin.username = :username", Question.class);
 		query.setParameter("username", username);
 		return query.getResultList();
+	}
+	
+	@Override
+	public List<Question> getSimilarQuestions(Long id) {
+		Question current = findById(id);
+		List<Question> others = findAll();
+		
+		
+		List<Question> similar = new ArrayList<Question>();
+		JaroWinkler jaroWinkler = new JaroWinkler();
+		for(Question question : others) {
+			if(jaroWinkler.getSimilarity(current.questionText, question.questionText) > 0.5) {
+				similar.add(question);
+			}
+		}
+		
+		return similar;
 	}
 
 }
