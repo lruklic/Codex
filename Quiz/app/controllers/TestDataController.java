41f7b39ab7303539a1889c1f8478c6027baf3056
Ruklić Luka <ruklic.luka@gmail.com>
2015-08-15 04:23:40 +0200

    Edited test controller. Added headings for pdf generator. Added css style for quiz display.

--- Quiz/app/controllers/TestDataController.java
+++ Quiz/app/controllers/TestDataController.java
@@ -37,29 +37,25 @@ public class TestDataController extends Controller {
 		permissions.add(Subject.GEOGRAPHY);
 		admin.subjectPermissions = permissions;
 		
+		admin.clearanceLevel = 3;
+		
 		userService.save(admin);
 		
-		Player player = new Player("player", "81dc9bdb52d04dc20036dbd8313ed055", "Luka", "Ruklić", "player@gmail.com");
+		
+		admin = new Admin("kkolak", "ca3e3f85c746511b5f881fe5054d2fc7", "Kruno", "Kolak", "kolak.kruno@gmail.com");
+		
+		permissions = new ArrayList<>();
+		permissions.add(Subject.HISTORY);
+		permissions.add(Subject.GEOGRAPHY);
+		admin.subjectPermissions = permissions;
+		
+		admin.clearanceLevel = 2;
+		
+		userService.save(admin);
+		
+		Player player = new Player("player", "81dc9bdb52d04dc20036dbd8313ed055", "Igrač", "Igralec", "player@gmail.com");
 		
 		userService.save(player);
-		
-//		Admin admin = new Admin("lruklic", "81dc9bdb52d04dc20036dbd8313ed055", "Luka", "Ruklić", "ruklic.luka@gmail.com");
-//		
-//		Player player = new Player("player", "81dc9bdb52d04dc20036dbd8313ed055", "Luka", "Ruklić", "player@gmail.com");
-//		
-//		userService.save(admin);
-//		userService.save(player);
-		
-//		Novelty novelty = new Novelty();
-//		novelty.noveltyTitle = "Dobro došli u Codex 0.1 !";
-//		novelty.noveltyText = "Prva verzija Codexa je <b><i>ONLINE</b></i>. Zasad je omogućeno dodavanje, pregled, "
-//				+ "uređivanje i brisanje pitanja. Implementirane su četiri različite vrste pitanja. <br><br> Slobodno "
-//				+ "isprobavajte i svakako prijavite bugove ako na njih naiđete.";
-//		novelty.newsPriority = NewsPriority.HIGH_PRIORITY;
-//		novelty.newsType = NewsType.FOR_EVERYONE;
-//		novelty.admin = (Admin) userService.findByUsernameOrEmail("lruklic");
-//		
-//		noveltyService.save(novelty);
 		
 		return ok();
 	}
