6f7e1efab92b807040031e9f772ba946371a451d
Ruklić Luka <ruklic.luka@gmail.com>
2015-10-22 02:30:42 +0200

    Added image option for question (not fully implemented).

--- Quiz/app/controllers/QuestionController.java
+++ Quiz/app/controllers/QuestionController.java
@@ -1,9 +1,11 @@
 package controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 import models.Admin;
+import models.Image;
 import models.Question;
 import models.User;
 import be.objectify.deadbolt.java.actions.Group;
@@ -16,6 +18,8 @@ import play.data.Form;
 import play.db.jpa.Transactional;
 import play.i18n.Messages;
 import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
 import services.model.QuestionService;
 import services.model.UserService;
@@ -52,21 +56,45 @@ public class QuestionController extends Controller {
 	 */
 	
 	public static Result submit() {
+		// Fetch question data from form
 		Form<QuestionForm> questionForm = Form.form(QuestionForm.class).bindFromRequest();
 		
 		String username = Session.getUsername();
 		User user = userService.findByUsernameOrEmail(username);
 		
+		// Validation
 		if(questionForm.hasErrors()) {
 			return badRequest(admin_question.render(questionForm, ((Admin)user).subjectPermissions, ModelCache.getInstance().getAllChapters(), ModelCache.getInstance().getAllGrades()));	// TODO check for admin twice? refactor!
 		}
 		
+		// Create question
 		Question question = null;
 		if (user instanceof Admin) {
 			 question = questionForm.get().createQuestion((Admin) user);	// if there are any errors, .get() will throw IllegalStateException: no value
 			 question.lastEdited = System.currentTimeMillis();
 		}
-
+		
+		// Get uploaded picture
+		MultipartFormData body = request().body().asMultipartFormData();
+		FilePart picture = body.getFile("image");
+		
+		if (picture != null) {
+		    String fileName = picture.getFilename();
+		    // String contentType = picture.getContentType();
+		    
+		    File file = picture.getFile();
+		    
+		    String newFilePath = "E:\\uploads\\" + fileName;	// put this value in properties
+		    
+		    file.renameTo(new File(newFilePath));
+		    
+		    Image image = new Image(newFilePath);
+		    question.image = image;
+		    
+		} else {
+		    flash("error", "Missing file");  
+		}
+		
 		questionService.save(question);
 
 		return redirect(routes.AdminController.adminList());
