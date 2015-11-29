7066565eb42c3874472a7f7e274608c57494b648
Ruklić Luka <ruklic.luka@gmail.com>
2015-11-29 23:36:41 +0100

    Player quiz picker has been redesigned.

--- Quiz/app/forms/QuizForm.java
+++ Quiz/app/forms/QuizForm.java
@@ -1,9 +1,28 @@
 package forms;
 
-import models.Subject;
+import java.util.List;
+
+/**
+ * Form that is instatiated and populated when user chooses a quiz to partake.
+ * 
+ * @author Luka Ruklic
+ *
+ */
 
 public class QuizForm {
+	/**
+	 * Grade which user chose.
+	 */
+	public String grade;
 	
-	public Subject subject;
+	/**
+	 * Subject which user chose.
+	 */
+	public String subject;
+	
+	/**
+	 * Chapter(s) of a subject that user chose.
+	 */
+	public List<String> chapters;
 
 }
